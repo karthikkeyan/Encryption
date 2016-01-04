94e22a85b404d957884dd53da50ce19ad0e24c55
Karthik Keyan <karthikkeyan.balan@gmail.com>
2016-01-04 13:36:29 +0530

    Initial Commit.

---
+++ Encryption/String+Encrypt.swift
@@ -0,0 +1,71 @@
+//
+//  String+Encrypt.swift
+//  CreditOne
+//
+//  Created by Karthik Keyan on 12/30/15.
+//  Copyright © 2015 Karthik Keyan. All rights reserved.
+//
+
+import Foundation
+import CommonCrypto
+
+enum HMACAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    func toCCEnum() -> CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:
+            result = kCCHmacAlgMD5
+        case .SHA1:
+            result = kCCHmacAlgSHA1
+        case .SHA224:
+            result = kCCHmacAlgSHA224
+        case .SHA256:
+            result = kCCHmacAlgSHA256
+        case .SHA384:
+            result = kCCHmacAlgSHA384
+        case .SHA512:
+            result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    func digestLength() -> Int {
+        var result: CInt = 0
+        switch self {
+        case .MD5:
+            result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:
+            result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:
+            result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:
+            result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:
+            result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:
+            result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+extension String {
+    
+    func encrypt(algorithm: HMACAlgorithm, key: String) -> String {
+        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let strLen = UInt(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLen = algorithm.digestLength()
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
+        let keyStr = key.cStringUsingEncoding(NSUTF8StringEncoding)
+        let keyLen = UInt(key.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        
+        CCHmac(algorithm.toCCEnum(), keyStr!, Int(keyLen), str!, Int(strLen), result)
+        let data = NSData(bytesNoCopy: result, length: digestLen)
+        result.destroy()
+        
+        return data.base64EncodedStringWithOptions(NSDataBase64EncodingOptions())
+    }
+    
+}
---
+++ Encryption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Encryption
+//
+//  Created by Karthik Keyan on 1/4/16.
+//  Copyright © 2016 Karthik Keyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
